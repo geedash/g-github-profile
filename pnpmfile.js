2b6c22ec73b4b4f609a92428f1d6006a9c4d3cda
Jeffrey Desir <i@jffrydsr.tech>
2020-12-16 22:30:19 -0500

    PUSH

---
+++ pnpmfile.js
@@ -0,0 +1,17 @@
+/* 
+  Version: 0.0.0.1
+*/
+
+import { pug } from "pug"; 
+import { generate } from "@appnest/readme";
+
+export const generateReadme = () => {
+   let MyGenerate = 
+    
+}
+
+
+module.export = {
+    generateReadme
+}
+
